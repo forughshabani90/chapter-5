c105815f5184abe6c70d32de9bdb1f9d6c5618ad
forughshabani90 <forugh.shabani.90@gmail.com>
2024-02-27 08:45:47 -0800

    first commit

---
+++ src/main/java/MultiplicationTable.java
@@ -0,0 +1,17 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println(" Multiplication Table");
+        System.out.println(" ");
+        for (int j=1;j<9;j++)
+            System.out.println(" "+j);
+        System.out.println("\n-----------------------");
+        for (int i = 1; i <=9; i++) {
+            System.out.println(i+"|");
+            for (int j = 1; j <=9; j++) {
+                System.out.println(i*j);
+            }
+            System.out.println();
+
+        }
+    }
+}
