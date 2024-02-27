cdc2f4f7d6fd5f298c96d911a8b12ac9e9e208f4
forughshabani90 <forugh.shabani.90@gmail.com>
2024-02-27 07:26:36 -0800

    first commit

---
+++ src/main/java/org/example/SubtractionQuizLoop.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SubtractionQuizLoop {
+    public static void main(String[] args) {
+        final int NUMBER_OF_QUESTIONS = 5;
+        int correctCount = 0;
+        int count = 0;
+        long startTime = System.currentTimeMillis();
+        String output = " ";
+        Scanner input = new Scanner(System.in);
+        while (count < NUMBER_OF_QUESTIONS) {
+            int number1 = (int) (Math.random() * 10);
+            int number2 = (int) (Math.random() * 10);
+            if (number1 < number2) {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+            System.out.print("What is " + number1 + " – " + number2 + "? ");
+            int answer = input.nextInt();
+
+            // 4. Grade the answer and display the result
+            if (number1 - number2 == answer) {
+                System.out.println("You are correct!");
+                correctCount++;
+            } else
+                System.out.println("Your answer is wrong.\n" + number1
+                        + " – " + number2 + " should be " + (number1 - number2));
+              count++;
+            output += "\n" + number1 + "–" + number2 + "=" + answer +
+                      ((number1 - number2 == answer) ? " correct": " wrong");
+        }
+        long endTime = System.currentTimeMillis();
+          long testTime = endTime - startTime;
+        System.out.println(correctCount+testTime/ 1000+ output);
+    }
+
+}
